4389cf30863dc1c0280a36456a90fd5803261895
AbraaoAlves <abraao.teodosio@gmail.com>
2024-02-18 17:19:13 +0000

    feat: starting project script

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! $MODEL_URL ]; then
+  MODEL_URL='https://github.com/dcTeam23/sentments-api/releases/download/model/best_model_state.bin'
+fi
+
+if [ ! -d "assets" ]; then
+  mkdir assets
+fi
+
+if [ ! -f "assets/best_model_state.bin" ]; then
+  wget -o assets/best_model_state.bin -c $MODEL_URL
+  echo "model downloaded"
+fi
+
+#Going to the src/api.py and referencing to the app
+uvicorn src.api:app --host 0.0.0.0
